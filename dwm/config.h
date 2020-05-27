16d34083e118160e565b2a528a08927903ed18a5
Kyryl Melekhin <k.melekhin@gmail.com>
2020-05-27 00:27:18 -0400

    mod dwm

--- dwm/config.h
+++ dwm/config.h
@@ -7,11 +7,11 @@ static const int showbar            = 0;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_gray1[]       = "#000000";
+static const char col_gray2[]       = "#BBBBBB";
+static const char col_gray3[]       = "#BBBBBB";
+static const char col_gray4[]       = "#EEEEEE";
+static const char col_cyan[]        = "#292929";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -76,6 +76,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_h,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_j,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_k,      setlayout,      {.v = &layouts[2]} },
+	{ MODKEY,                       XK_n,      togglefullscr,  {0} },
 	//{ MODKEY,                       XK_space,  setlayout,      {0} },
 	//{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
