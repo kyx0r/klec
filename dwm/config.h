e8a87d717934566dbc545a795d172450f75b0ea5
Kyryl Melekhin <k.melekhin@gmail.com>
2020-06-03 10:03:34 -0400

    ststatus

--- dwm/config.h
+++ dwm/config.h
@@ -60,11 +60,15 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *slstatus[]  = { "slstatus", NULL };
+static const char *slkill[]  = { "pkill", "slstatus", NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
 	{ MODKEY,                       XK_t,      spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_F1,     spawn,          {.v = slstatus } },
+	{ MODKEY,                       XK_F2,     spawn,          {.v = slkill } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_x,      focusstack,     {.i = +1 } },
 	//{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -73,18 +77,14 @@ static Key keys[] = {
 	//{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	//{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
 	//{ MODKEY,                       XK_Return, zoom,           {0} },
-	//{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,                       XK_z,      killclient,     {0} },
 	{ MODKEY,                       XK_h,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_j,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_k,      setlayout,      {.v = &layouts[2]} },
-//	{ MODKEY,                       XK_u,      setlayout,      {.v = &layouts[3]} },
-//	{ MODKEY,                       XK_o,      setlayout,      {.v = &layouts[4]} },
 	{ MODKEY,                       XK_c,      swapfocus,      {.i = -1 } },
 	{ MODKEY,           		XK_s,      cyclelayout,    {.i = +1 } },
 	{ MODKEY,                       XK_m,      togglefullscr,  {0} },
-	//{ MODKEY,                       XK_space,  setlayout,      {0} },
-	{ MODKEY,                       XK_n,  togglefloating, {0} },
+	{ MODKEY,                       XK_n,      togglefloating, {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
--- dwm/dwm.c
+++ dwm/dwm.c
@@ -748,6 +748,9 @@ drawbar(Monitor *m)
 	}
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++) {
+		/* do not draw vacant tags */
+		if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
+		continue;
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
