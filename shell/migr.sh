510fec219daaf5687f826b6cb1b28e3c9915d39d
Kyryl Melekhin <k.melekhin@gmail.com>
2020-06-08 12:28:21 +0000

    migr.sh

---
+++ shell/migr.sh
@@ -0,0 +1,22 @@
+#
+#!/bin/bash
+#Migrates root file system to RAM
+#
+
+mkdir -p /ramroot
+mount -o size=8G -t tmpfs tmpfs /ramroot
+cp -a /{bin,etc,sbin,lib64,lib32,lib} /ramroot
+mkdir /ramroot/usr
+cp -a /usr/{bin,sbin,lib32,lib64,lib,share,include,local} /ramroot/usr &
+cp -a /{root,home,var} /ramroot &
+umount /boot
+mkdir /ramroot/{dev,proc,sys,run,tmp}
+mount --rbind /sys /ramroot/sys
+mount --rbind /run /ramroot/run
+mount --rbind /tmp /ramroot/tmp
+mount --rbind /dev /ramroot/dev
+mount --rbind /proc /ramroot/proc
+#mount -t proc none /ramroot/proc
+mkdir /ramroot/oldroot
+wait < <(jobs -p)
+pivot_root /ramroot /ramroot/oldroot
