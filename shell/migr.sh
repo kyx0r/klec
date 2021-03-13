281ae2d2ad7150e6ccd3744e1c92f46208d66adc
Kyryl Melekhin <k.melekhin@gmail.com>
2021-03-13 14:22:24 +0000

    add tmpfs to migr

--- shell/migr.sh
+++ shell/migr.sh
@@ -1,32 +1,21 @@
 #!/bin/sh
 # Migrates root file system to RAM
-# --- Needs busybox version of pivot_root ---
 # run it like so . migr.sh the "." is does the shell sourcing
 # or make sure to run cd / yourself. It needs to become aware of
 # the new root by changing shell wd.
 # The /sys/block/zram0/disksize does not really matter what size
 # it's just a limiter, so set to anything except below the amount
-# of your ram. The kernel does not do deallocations when you remove
-# files from the zram block, to reset it run "echo 1 > /sys/block/zram0/reset"
-# ignore the busy error, it works either way. 
+# of your ram.
 # You may need to adjust the script directories, but the most
 # essential ones are alredy here to have a working system.
 #
-# You can also do this onto tmpfs instead of zram,
-# just uncomment the line with tmpfs and comment the 
-# mkfs.ext4 and the zram stuff
+# You can also do this onto tmpfs instead of zram
 
 usrdirs="bin sbin lib32 lib64 lib share include local libexec"
 rootdirs="etc root home var"
 
 function copy()
 {
-	mkdir -p /ramroot
-	modprobe zram num_devices=1
-	echo "32G" > /sys/block/zram0/disksize
-	mkfs.ext4 /dev/zram0
-	mount /dev/zram0 /ramroot
-	#mount -o size=8G -t tmpfs tmpfs /ramroot
 	cp -a /bin /ramroot
 	cp -a /sbin /ramroot
 	cp -a /lib64 /ramroot
@@ -43,6 +32,23 @@ function copy()
 	/ramroot/run /ramroot/tmp /ramroot/cache /ramroot/oldroot
 	echo "please wait for job completion ..."
 	exit 0
+}
+
+function tcopy()
+{
+	mkdir -p /ramroot
+	mount -o size=32G -t tmpfs tmpfs /ramroot
+	copy()
+}
+
+function zcopy()
+{
+	mkdir -p /ramroot
+	modprobe zram num_devices=1
+	echo "32G" > /sys/block/zram0/disksize
+	mkfs.ext4 /dev/zram0
+	mount /dev/zram0 /ramroot
+	copy()
 }
 
 function pivot()
@@ -63,7 +69,8 @@ function help()
 {
 	echo " "
         echo "=====Command Line Options====="
-        echo "* copy            copy rootfs system to ram"
+        echo "* zcopy           copy rootfs system to ram (zram0)"
+        echo "* tcopy           copy rootfs system to ram (tmpfs)"
         echo "* pivot           pivot rootfs"
 }
 
