bfd7d8b74aed93c73aa8e03b599217f2a47edc9d
Kyryl Melekhin <k.melekhin@gmail.com>
2021-05-10 17:03:03 +0000

    st: add dynamic cursor color

--- cprogs/st/x.c
+++ cprogs/st/x.c
@@ -1501,10 +1501,11 @@ void
 xdrawcursor(int cx, int cy, Glyph g, int ox, int oy, Glyph og)
 {
 	Color drawcol;
+	XRenderColor colbg;
 
 	/* remove the old cursor */
 	if (selected(ox, oy))
-                og.mode ^= ATTR_SELECTED;
+		og.mode ^= ATTR_SELECTED;
 	xdrawglyph(og, ox, oy);
 
 	if (IS_SET(MODE_HIDE))
@@ -1517,14 +1518,23 @@ xdrawcursor(int cx, int cy, Glyph g, int ox, int oy, Glyph og)
 
 	if (IS_SET(MODE_REVERSE)) {
 		g.mode |= ATTR_REVERSE;
-                g.fg = defaultcs;
+		g.fg = defaultcs;
 		g.bg = defaultfg;
-                drawcol = dc.col[defaultrcs];
+		drawcol = dc.col[defaultrcs];
 	} else {
-                g.fg = defaultbg;
-                g.bg = defaultcs;
-                drawcol = dc.col[defaultcs];
+		g.bg = g.fg;
+		g.fg = defaultbg;
+		drawcol = dc.col[defaultcs];
 	}
+
+	if (IS_TRUECOL(g.bg)) {
+		colbg.alpha = 0xffff;
+		colbg.red = TRUERED(g.bg);
+		colbg.green = TRUEGREEN(g.bg);
+		colbg.blue = TRUEBLUE(g.bg);
+		XftColorAllocValue(xw.dpy, xw.vis, xw.cmap, &colbg, &drawcol);
+	} else
+		drawcol = dc.col[g.bg];
 
 	/* draw the new one */
 	if (IS_SET(MODE_FOCUSED)) {
