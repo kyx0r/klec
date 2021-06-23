adcbc17e3f8d32da8ad64efec3b3078c530d6c47
Kyryl Melekhin <k.melekhin@gmail.com>
2021-06-23 22:44:41 +0000

    dwm: call configure when fullscreen requested

--- cprogs/dwm/dwm.c
+++ cprogs/dwm/dwm.c
@@ -590,9 +590,11 @@ clientmessage(XEvent *e)
 		return;
 	if (cme->message_type == netatom[NetWMState]) {
 		if (cme->data.l[1] == netatom[NetWMFullscreen]
-		|| cme->data.l[2] == netatom[NetWMFullscreen])
+		|| cme->data.l[2] == netatom[NetWMFullscreen]) {
 			setfullscreen(c, (cme->data.l[0] == 1 /* _NET_WM_STATE_ADD    */
 				|| cme->data.l[0] == 2 /* _NET_WM_STATE_TOGGLE */));
+			configure(c);
+		}
 	} else if (cme->message_type == netatom[NetActiveWindow]) {
 		if (c != selmon->sel && !c->isurgent)
 			seturgent(c, 1);
