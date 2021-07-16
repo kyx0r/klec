d7cfb621b456a7c296c232734ac9052fb522396e
Kyryl Melekhin <k.melekhin@gmail.com>
2021-07-16 19:23:01 +0000

    install: use symlinks

--- install.sh
+++ install.sh
@@ -4,21 +4,21 @@ cp cgi/ten_years__time_by_bythmark_d4dg6dz ~/
 cp -r cgi/GoldenShimmer /usr/share/icons
 cp -r cgi/terminus-ttf-4.47.0/ /usr/share/fonts
 
-cp dotfiles/.twmrc ~/
-cp dotfiles/.bashrc ~/
-cp dotfiles/.profile ~/
-cp dotfiles/.rc ~/
-cp dotfiles/.gdbinit ~/
-cp dotfiles/.vimrc ~/
-cp dotfiles/.gitconfig ~/
-cp dotfiles/.mailrc ~/
-cp dotfiles/.radare2rc ~/
-cp dotfiles/lynx_bookmarks.html ~/
-cp dotfiles/keynavrc /etc
-cp dotfiles/lynx.cfg /etc
-cp dotfiles/index.theme /usr/share/icons/default/
-cp dotfiles/local.conf /etc/fonts
-cp dotfiles/grub /etc/default/
+ln -sf $PWD/dotfiles/.twmrc ~/
+ln -sf $PWD/dotfiles/.bashrc ~/
+ln -sf $PWD/dotfiles/.profile ~/
+ln -sf $PWD/dotfiles/.rc ~/
+ln -sf $PWD/dotfiles/.gdbinit ~/
+ln -sf $PWD/dotfiles/.vimrc ~/
+ln -sf $PWD/dotfiles/.gitconfig ~/
+ln -sf $PWD/dotfiles/.mailrc ~/
+ln -sf $PWD/dotfiles/.radare2rc ~/
+ln -sf $PWD/dotfiles/lynx_bookmarks.html ~/
+ln -sf $PWD/dotfiles/keynavrc /etc
+ln -sf $PWD/dotfiles/lynx.cfg /etc
+ln -sf $PWD/dotfiles/index.theme /usr/share/icons/default/
+ln -sf $PWD/dotfiles/local.conf /etc/fonts
+ln -sf $PWD/dotfiles/grub /etc/default/
 
 cd cprogs
 cd st
