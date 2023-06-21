06ad3fb7545e5c4f6566e9f1e962d4673fc2fe1a
Kyryl Melekhin <k.melekhin@gmail.com>
2023-06-21 11:39:36 -0400

    keynavrc, lynx_bookmarks.html: rm

--- install.sh
+++ install.sh
@@ -9,8 +9,6 @@ ln -sf $PWD/dotfiles/.rc ~/
 ln -sf $PWD/dotfiles/.gdbinit ~/
 ln -sf $PWD/dotfiles/.vimrc ~/
 ln -sf $PWD/dotfiles/.gitconfig ~/
-ln -sf $PWD/dotfiles/lynx_bookmarks.html ~/
-ln -sf $PWD/dotfiles/keynavrc /etc
 ln -sf $PWD/dotfiles/lynx.cfg /etc
 ln -sf $PWD/dotfiles/index.theme /usr/share/icons/default/
 ln -sf $PWD/dotfiles/local.conf /etc/fonts
