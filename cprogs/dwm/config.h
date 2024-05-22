b7f85395c295b304e62292fdf53efe34784d0eba
Kyryl Melekhin <k.melekhin@gmail.com>
2024-05-21 20:25:51 -0400

    cprogs/dwm/config.h: floating last

--- cprogs/dwm/config.h
+++ cprogs/dwm/config.h
@@ -39,9 +39,9 @@ static const int resizehints = 1;    /* 1 means respect size hints in tiled resi
 static const Layout layouts[] = {
 	/* symbol     arrange function */
 	{ "[]=",      tile },    /* first entry is default */
-	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
 	{ "[D]",      deck },
+	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ NULL,       NULL },
 };
 
