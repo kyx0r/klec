f3de26b2e2d0e2dba4feb14a1c83f72f93ac345f
Kyryl Melekhin <k.melekhin@gmail.com>
2025-10-10 00:12:35 +0000

    linux 6.17.1

--- shell/efistub.sh
+++ shell/efistub.sh
@@ -6,6 +6,6 @@ efibootmgr \
          --disk     /dev/nvme0n1 \
          --part     1 \
          --label    KISS \
-         --loader   /vmlinuz-6.16.0 \
+         --loader   /vmlinuz-6.17.1 \
          --unicode  'root=/dev/nvme0n1p2 amdgpu.ppfeaturemask=0xffffffff' \
          --verbose
