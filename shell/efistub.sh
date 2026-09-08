c4b4ac9504e6f5d601cb8eedd15efe1c0098d718
Kyryl Melekhin <k.melekhin@gmail.com>
2026-09-08 17:47:59 +0000

    update kernel config

--- shell/efistub.sh
+++ shell/efistub.sh
@@ -6,6 +6,6 @@ efibootmgr \
          --disk     /dev/nvme0n1 \
          --part     1 \
          --label    KISS \
-         --loader   /vmlinuz-7.0.8 \
+         --loader   /vmlinuz-7.2.4 \
          --unicode  'root=/dev/nvme0n1p2 amdgpu.ppfeaturemask=0xffffffff' \
          --verbose
